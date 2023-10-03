7743334b9f44c5dff01d52c87d0d346024ac0d24
Dylan C <Dylan C@DESKTOP-UVOOFLP>
2023-10-03 14:54:40 -0600

    Practica 1

---
+++ src/main/java/com/tiendaIsa/TiendaIsaApplication.java
@@ -0,0 +1,13 @@
+package com.tiendaIsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaIsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaIsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendaIsa/TiendaIsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendaIsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaIsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
