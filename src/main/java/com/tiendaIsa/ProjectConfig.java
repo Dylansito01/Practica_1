00663a3b9274d3dcd576633fcc0651269d40654c
Dylan C <Dylan C@DESKTOP-UVOOFLP>
2023-11-21 15:47:47 -0600

    Avance #3

---
+++ src/main/java/com/tiendaIsa/ProjectConfig.java
@@ -0,0 +1,43 @@
+
+package com.tiendaIsa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+
+public class ProjectConfig {
+    
+    @Bean
+    public UserDetailsService users() {
+        UserDetails admin = User.builder()
+                .username("juan")
+                .password("{noop}123")
+                .roles("USER", "VENDEDOR", "ADMIN")
+                .build();
+        UserDetails sales = User.builder()
+                .username("rebeca")
+                .password("{noop}456")
+                .roles("USER", "VENDEDOR")
+                .build();
+        UserDetails user = User.builder()
+                .username("pedro")
+                .password("{noop}789")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user, sales, admin);
+    }
+          @Autowired
+      private UserDetailsService userDetailsService;
+      
+      @Autowired
+      public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception{
+          build.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+      }
+    
+}
